cc078607c937c012c5c105aad3c7883c6ead0953
AlvaroTreal <alvaro_js@outlook.com>
2024-06-06 17:49:56 -0300

    alteracao docker-composer

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require('express'),
     // Mitigate XSS using sanitizer
     sanitizer = require('sanitizer'),
     app = express(),
-    port = 7001
+    port = 7003
 
 app.use(bodyParser.urlencoded({
     extended: false
